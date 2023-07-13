4ba3144c229cf9e79737e4840be1dd1eb9bf905c
Fihliwe <siyabulelmncube@gmail.com>
2023-07-13 10:31:51 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * Main - Programming is like building a multilingual puzzle
  * return value = 0
  */
-int main(void){
-	puts("Programming is like building a multilingual puzzle");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
