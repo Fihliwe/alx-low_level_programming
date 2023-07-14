a37470ae6938a0698661c3745056acd706acf2d7
Fihliwe <siyabulelmncube@gmail.com>
2023-07-14 06:52:37 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  program that prints the alphabet in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+char lower;
+
+lower = 'a';
+while
+	(lower <= 'z' && lower != 'q', 'e') {
+		putchar(lower);
+		lower++;
+	}
+putchar('\n');
+return (0);
+}
