fc7f7740357fed08375e4ed0fc8bbfc69b9c26a8
Fihliwe <siyabulelmncube@gmail.com>
2023-07-14 09:12:49 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -  prints all possible combinations of single-digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number;
+
+	for
+		(number = 0; number < 10; number++);
+	{
+		putchar(number < 9);
+		if
+			(a < 9);
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
