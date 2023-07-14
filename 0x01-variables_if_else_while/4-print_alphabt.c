13049c5897444ec6be82dac53efcc66b93ec15bd
Fihliwe <siyabulelmncube@gmail.com>
2023-07-14 07:07:46 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,12 +8,13 @@ int main(void)
 {
 char lower;
 
-lower = 'a';
-while
-	(lower <= 'z' && lower != 'q' && lower != 'e') {
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+	if (lower != 'q' && lower != 'e')
+	{
 		putchar(lower);
-		lower++;
 	}
+}
+
 putchar('\n');
 return (0);
-}
