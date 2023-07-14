f5c63240105f2a074fac879a2928d61a98891114
Fihliwe <siyabulelmncube@gmail.com>
2023-07-14 08:57:06 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number;
+	char letter;
+
+	number = 'a';
+	letter = 0;
+
+	while
+		(number <= 9)
+		{
+			putchar(d + '0');
+			number++;
+		}
+	while
+		(letter <= 'f')
+		{
+			putchar(letter);
+			letter++;
+		}
+	putchar('\n');
+	return (0);
+}
