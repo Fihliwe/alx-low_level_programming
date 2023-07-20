1f7cac047a7a3f0d96fd526938ae16087764e14e
Fihliwe <siyabulelmncube@gmail.com>
2023-07-20 03:00:13 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_number -  that prints the numbers, from 0 to 9
+ */
+
+void print_most_numbers(void)
+{
+	char n;
+
+	for ( n = '0', n <= '9', c++)
+	{
+		if(!(n =='2' || n == '4'))
+			_putchar(n);
+	}
+	_putchar('\n');
+}
