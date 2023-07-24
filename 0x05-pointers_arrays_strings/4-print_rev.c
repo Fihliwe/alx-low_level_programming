a1478cdf66c093e5fa0bcba7420924dd47382c47
Fihliwe <siyabulelmncube@gmail.com>
2023-07-24 01:28:19 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,8 +6,22 @@
  * return: 0
  */
 
-void print_rev(char *s)
 {
-	strrev(*s);
-	_putchar(strrev);
+	int length = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	s--;
+	for (o = length; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
 }
