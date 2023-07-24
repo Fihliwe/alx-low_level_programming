596d85d0b6ad3442bb5aabed078762beff9e5c4c
Fihliwe <siyabulelmncube@gmail.com>
2023-07-24 01:21:11 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+
+#include "main.h"
+/**
+ * print_rev -  that prints a string, in reverse, followed by a new line
+ * @s: string
+ * return: 0
+ */
+
+void print_rev(char *s)
+{
+	strrev(*s);
+	_putchar(strrev);
+}
