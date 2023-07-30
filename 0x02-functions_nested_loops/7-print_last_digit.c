1c772841e956124c2bcb9300809ec79dcda090ec
Fihliwe <siyabulelmncube@gmail.com>
2023-07-30 12:01:35 -0700

    print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * print_last_digit -  function that prints the last digit of a number.
- * @n - prints the random number
- * Return: value of last digit ('l_digit')
+ * @n: prints the random number
+ * Return: l_digit
  */
 int print_last_digit(int n)
 {
