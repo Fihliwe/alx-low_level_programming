5d667ebaccdf6ff55db3c0f38c5087b532127009
Fihliwe <siyabulelmncube@gmail.com>
2023-08-14 03:38:30 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib>
+
+/**
+ * main - program will be runned
+ * printf: function will be used to print file name
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s/n", __FILE__);
+
+	return (0);
+}
