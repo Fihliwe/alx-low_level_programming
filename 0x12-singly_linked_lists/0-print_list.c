77905ce794d2eae4b68059a2de61b78c4edba720
Fihliwe <siyabulelmncube@gmail.com>
2023-08-24 03:51:58 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "list.h"
+
+/**
+ * print_list - prints all the elements 
+ * @h: head
+ * Return: character
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t num_elements;
+	num_elements = 0;
+
+	while (h)
+	{
+		if (!h -> str)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%u] %s\n", h->len, h->str);
+			h = h -> next;
+			num_elements++;
+	}
+	return (num_elements);
+}
