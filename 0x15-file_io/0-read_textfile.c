85f3573417c6427b827a8e619eb9c719c6243c19
Fihliwe <siyabulelmncube@gmail.com>
2023-09-04 05:24:58 -0700

    file handling

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,18 +1,13 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - a text file and prints it to the POSIX standard output
- * @filename: the name of the file
- * @letter: the letter printed
- * Return: 0
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
